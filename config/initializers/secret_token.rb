e008f25c97f136ea4dfd8de977f2fdaf32dc1324
parkh <da.parho@gmail.com>
2013-06-07 18:35:23 +0300

    audiohub scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Audiohub::Application.config.secret_key_base = '301d017bfb2a43db822289b55197c94181266a3113f845fba24a9892f7e1ac5f2491454d65a3e316b87754762680331d9e3f3c4d632f47cbc8b92b5718ac97a1'
